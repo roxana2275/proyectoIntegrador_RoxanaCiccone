f095898d674362bbb3966d0c41ecb4d14dc0c649
roxana2275 <roxana.ciccone@gmail.com>
2023-04-17 19:19:53 -0300

    comienzo backend

---
+++ BackEnd/src/test/java/com/portFolio/rc/RcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portFolio.rc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
